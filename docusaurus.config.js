633eaae01e7cebd698b6fd4308095f6e41c49cdb
Unix101 <unixsystem10@gmail.com>
2021-07-06 23:48:56 -0400

    Remove blog 👀

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -25,13 +25,6 @@ module.exports = {
           docId: 'intro',
           position: 'left',
           label: 'Tutorial',
-        },
-        {to: '/blog', label: 'Blog', position: 'left'},
-        {
-          href: 'https://github.com/facebook/docusaurus',
-          label: 'GitHub',
-          position: 'right',
-        },
       ],
     },
     footer: {
@@ -89,13 +82,6 @@ module.exports = {
           // Please change this to your repo.
           editUrl:
             'https://github.com/facebook/docusaurus/edit/master/website/',
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
-        },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
